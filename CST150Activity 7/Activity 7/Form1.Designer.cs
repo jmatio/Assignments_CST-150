5338162db182860110700f83eab4df6cf07ffb3c
jmatio <113790485+jmatio@users.noreply.github.com>
2022-10-02 14:55:07 -0800

    Activity 7

---
+++ CST150Activity 7/Activity 7/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Activity_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.enterTermsLabel = new System.Windows.Forms.Label();
+            this.inputTermsTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.outputCalculationLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enterTermsLabel
+            // 
+            this.enterTermsLabel.AutoSize = true;
+            this.enterTermsLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.enterTermsLabel.Location = new System.Drawing.Point(34, 48);
+            this.enterTermsLabel.Name = "enterTermsLabel";
+            this.enterTermsLabel.Size = new System.Drawing.Size(127, 20);
+            this.enterTermsLabel.TabIndex = 0;
+            this.enterTermsLabel.Text = "Enter # of terms:";
+            // 
+            // inputTermsTextBox
+            // 
+            this.inputTermsTextBox.Location = new System.Drawing.Point(167, 45);
+            this.inputTermsTextBox.Name = "inputTermsTextBox";
+            this.inputTermsTextBox.Size = new System.Drawing.Size(136, 26);
+            this.inputTermsTextBox.TabIndex = 1;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(38, 88);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(265, 35);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // outputCalculationLabel
+            // 
+            this.outputCalculationLabel.AutoSize = true;
+            this.outputCalculationLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.outputCalculationLabel.Location = new System.Drawing.Point(24, 149);
+            this.outputCalculationLabel.Name = "outputCalculationLabel";
+            this.outputCalculationLabel.Size = new System.Drawing.Size(279, 60);
+            this.outputCalculationLabel.TabIndex = 3;
+            this.outputCalculationLabel.Text = "Approximate value of pi after \"#\" terms\r\n\r\n= \"Solution\"\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(348, 266);
+            this.Controls.Add(this.outputCalculationLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.inputTermsTextBox);
+            this.Controls.Add(this.enterTermsLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Approximate PI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label enterTermsLabel;
+        private System.Windows.Forms.TextBox inputTermsTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label outputCalculationLabel;
+    }
+}
+
---
+++ CST150Activity 7/Activity 7/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //calculates the input on click
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //initializes variables
+                double input = Convert.ToDouble(inputTermsTextBox.Text);
+                int odd = 1;
+                double pi = 0.0;
+
+                //runs through loop and calculates pi based on number of terms given
+                for (int i = 1; i <= input; i++)
+                {
+                    double currentTerm = 0.0;
+
+                    //checks if even or odd and calculates correct pi formula
+                    if (i % 2 == 0)
+                    {
+                        currentTerm = (double)-4 / odd;
+                    }
+                    else
+                    {
+                        currentTerm = (double)4 / odd;
+                    }
+
+                    //updates variables
+                    odd = odd + 2;
+                    pi = pi + currentTerm;
+
+
+                }
+
+                //outputs result to textbox
+                outputCalculationLabel.Text = "Approximate value of pi after " + inputTermsTextBox.Text + " terms \n = " + pi.ToString();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
