e21223047884506d29027b27ba57b30de8c8b78e
jmatio <113790485+jmatio@users.noreply.github.com>
2022-10-09 12:29:51 -0700

    CST150 Milestone 2

---
+++ CST150Milestone 2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Milestone2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //method that will execute when add button is clicked
+        public void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //implements the InventoryItem class and creates a new object
+                InventoryItem item = new InventoryItem();
+
+                //sets values to object from user input
+                item.Name = nameTextBox.Text;
+                item.Department = departmentTextBox.Text;
+                item.PartNumber = Convert.ToInt32(partNumberTextBox.Text);
+                item.Price = Convert.ToDecimal(priceTextBox.Text);
+                item.Quantity = Convert.ToInt32(quantityTextBox.Text);
+
+                //adds object to listbox
+                inventoryListBox.Items.Add(item);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(Text, ex.Message);
+            }
+        }
+
+        //method that will execute when remove button is clicked
+        public void btnRemove_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //removes the selected item from the listbox
+                inventoryListBox.Items.RemoveAt(inventoryListBox.SelectedIndex);
+            }
+            catch
+            {
+                MessageBox.Show("Must select an item for removal");
+            }
+        }
+    }
+}
---
+++ CST150Milestone 2/InventoryItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone2
+{
+    //defines the InventoryItem class
+    internal class InventoryItem
+    {
+        //sets variables with getters and setters
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public int PartNumber { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        //method to convert all outputs to string and orders them how they will appear in list box
+        public override string ToString()
+        {
+            return Name + ", " + Department + ", " + PartNumber + ", " + Price + ", " + Quantity + ".";
+        }
+    }
+}
