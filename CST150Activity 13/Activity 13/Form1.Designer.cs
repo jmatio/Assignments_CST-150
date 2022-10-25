3241ac52370e2ce99fba1c023805d4ed11cecb4a
jmatio <113790485+jmatio@users.noreply.github.com>
2022-10-24 21:48:05 -0700

    Activity 13

---
+++ CST150Activity 13/Activity 13/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Activity_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.simulateButton = new System.Windows.Forms.Button();
+            this.checkScoresButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(465, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitMenu});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitMenu
+            // 
+            this.exitMenu.Name = "exitMenu";
+            this.exitMenu.Size = new System.Drawing.Size(270, 34);
+            this.exitMenu.Text = "Exit";
+            this.exitMenu.Click += new System.EventHandler(this.exitMenu_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutMenu});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(65, 29);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutMenu
+            // 
+            this.aboutMenu.Name = "aboutMenu";
+            this.aboutMenu.Size = new System.Drawing.Size(270, 34);
+            this.aboutMenu.Text = "About";
+            this.aboutMenu.Click += new System.EventHandler(this.aboutMenu_Click);
+            // 
+            // A1
+            // 
+            this.A1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A1.Location = new System.Drawing.Point(73, 138);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(100, 100);
+            this.A1.TabIndex = 1;
+            this.A1.UseVisualStyleBackColor = true;
+            // 
+            // A2
+            // 
+            this.A2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A2.Location = new System.Drawing.Point(179, 138);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(100, 100);
+            this.A2.TabIndex = 2;
+            this.A2.UseVisualStyleBackColor = true;
+            // 
+            // A3
+            // 
+            this.A3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A3.Location = new System.Drawing.Point(285, 138);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(100, 100);
+            this.A3.TabIndex = 3;
+            this.A3.UseVisualStyleBackColor = true;
+            // 
+            // B1
+            // 
+            this.B1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B1.Location = new System.Drawing.Point(73, 244);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(100, 100);
+            this.B1.TabIndex = 4;
+            this.B1.UseVisualStyleBackColor = true;
+            // 
+            // B2
+            // 
+            this.B2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B2.Location = new System.Drawing.Point(179, 244);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(100, 100);
+            this.B2.TabIndex = 5;
+            this.B2.UseVisualStyleBackColor = true;
+            // 
+            // B3
+            // 
+            this.B3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B3.Location = new System.Drawing.Point(285, 244);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(100, 100);
+            this.B3.TabIndex = 6;
+            this.B3.UseVisualStyleBackColor = true;
+            // 
+            // C1
+            // 
+            this.C1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C1.Location = new System.Drawing.Point(73, 350);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(100, 100);
+            this.C1.TabIndex = 7;
+            this.C1.UseVisualStyleBackColor = true;
+            // 
+            // C2
+            // 
+            this.C2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C2.Location = new System.Drawing.Point(179, 350);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(100, 100);
+            this.C2.TabIndex = 8;
+            this.C2.UseVisualStyleBackColor = true;
+            // 
+            // C3
+            // 
+            this.C3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C3.Location = new System.Drawing.Point(285, 350);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(100, 100);
+            this.C3.TabIndex = 9;
+            this.C3.UseVisualStyleBackColor = true;
+            // 
+            // simulateButton
+            // 
+            this.simulateButton.Location = new System.Drawing.Point(73, 487);
+            this.simulateButton.Name = "simulateButton";
+            this.simulateButton.Size = new System.Drawing.Size(312, 36);
+            this.simulateButton.TabIndex = 10;
+            this.simulateButton.Text = "Simulate";
+            this.simulateButton.UseVisualStyleBackColor = true;
+            this.simulateButton.Click += new System.EventHandler(this.simulateButton_Click);
+            // 
+            // checkScoresButton
+            // 
+            this.checkScoresButton.Location = new System.Drawing.Point(73, 535);
+            this.checkScoresButton.Name = "checkScoresButton";
+            this.checkScoresButton.Size = new System.Drawing.Size(312, 36);
+            this.checkScoresButton.TabIndex = 11;
+            this.checkScoresButton.Text = "Check Score";
+            this.checkScoresButton.UseVisualStyleBackColor = true;
+            this.checkScoresButton.Click += new System.EventHandler(this.checkScoresButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(465, 610);
+            this.Controls.Add(this.checkScoresButton);
+            this.Controls.Add(this.simulateButton);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitMenu;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutMenu;
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button simulateButton;
+        private System.Windows.Forms.Button checkScoresButton;
+    }
+}
+
---
+++ CST150Activity 13/Activity 13/Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_13
+{
+    public partial class Form1 : Form
+    {
+        //creates a 2d array and initializes with 3 rows and 3 columns
+        int[,] gameArray = new int[3,3];
+
+        int row, col;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //method checks all score variables
+        public void checkScore()
+        {
+
+            int firstPlayer = 0;
+            int secondPlayer = 0;
+
+            //checks first row
+
+            if (A1.Text != "" && A2.Text != "" && A3.Text != "")
+            {
+
+                if (A1.Text == A2.Text && A2.Text == A3.Text)
+                {
+                    if (A1.Text == "X")
+                    {
+                        firstPlayer++;
+                    }
+
+                    else
+                    {
+                        secondPlayer++;
+                    }
+                }    
+            }
+
+            //checks second row
+
+            if (B1.Text != "" && B2.Text != "" && B3.Text != "")
+            {
+
+                if (B1.Text == B2.Text && B2.Text == B3.Text)
+                {
+
+                    if (B1.Text == "X")
+                    {
+                        firstPlayer++;
+                    }
+
+                    else
+                    {
+                        secondPlayer++;
+                    }
+                }
+            }
+
+            //checks third row
+
+            if (C1.Text != "" && C2.Text != "" && C3.Text != "")
+            {
+
+                if (C1.Text == C2.Text && C2.Text == C3.Text)
+                {
+
+                    if (C1.Text == "X")
+                    {
+                        firstPlayer++;
+                    }
+
+                    else
+                    {
+                        secondPlayer++;
+                    }
+                }
+            }
+
+            //checks top left to bottom right diagonal
+
+            if (A1.Text != "" && B2.Text != "" && C3.Text != "")
+            {
+                
+                if (A1.Text == B2.Text && B2.Text == C3.Text)
+                {
+
+                    if (A1.Text == "X")
+                    {
+                        firstPlayer++;
+                    }
+
+                    else
+                    {
+                        secondPlayer++;
+                    }
+                }
+            }
+
+            //checks top right to bottom left diagonal
+
+            if (A3.Text != "" && B2.Text != "" && C1.Text != "")
+            {
+
+                if (A3.Text == B2.Text && B2.Text == C1.Text)
+                {
+
+                    if (A3.Text == "X")
+                    {
+                        firstPlayer++;
+                    }
+
+                    else
+                    {
+                        secondPlayer++;
+                    }
+                }
+            }
+
+            //checks first column
+
+            if (A1.Text != "" && B1.Text != "" && C1.Text != "")
+            {
+
+                if (A1.Text == B1.Text && B1.Text == C1.Text)
+                {
+
+                    if (A1.Text == "X")
+                    {
+                        firstPlayer++;
+                    }
+
+                    else
+                    {
+                        secondPlayer++;
+                    }
+                }
+            }
+
+            //checks second column
+
+            if (A2.Text != "" && B2.Text != "" && C2.Text != "")
+            {
+
+                if (A2.Text == B2.Text && B2.Text == C2.Text)
+                {
+
+                    if (A2.Text == "X")
+                    {
+                        firstPlayer++;
+                    }
+
+                    else
+                    {
+                        secondPlayer++;
+                    }
+                }
+            }
+
+            //checks third column
+
+            if (A3.Text != "" && B3.Text != "" && C3.Text != "")
+            {
+
+                if (A3.Text == B3.Text && B3.Text == C3.Text)
+                {
+
+                    if (A3.Text == "X")
+                    {
+                        firstPlayer++;
+                    }
+
+                    else
+                    {
+                        secondPlayer++;
+                    }
+                }
+            }
+
+            //checks score to determine which player won the game and displays winner
+
+            if (firstPlayer == secondPlayer)
+            {
+                MessageBox.Show("It's a Draw!");
+            }
+
+            else if (firstPlayer > secondPlayer)
+            {
+                MessageBox.Show("Player One Wins!");
+            }
+
+            else if (firstPlayer < secondPlayer)
+            {
+                MessageBox.Show("Player Two Wins!");
+            }
+
+        }
+
+        //displays when "About" button is clicked in menu
+        private void aboutMenu_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Justin Matioschat");
+        }
+
+        //simulates a game of tic tac toe on click
+        private void simulateButton_Click(object sender, EventArgs e)
+        {
+
+            Random random = new Random();
+
+            for (row = 0; row < 3; row++)
+            {
+
+                for (col = 0; col < 3; col++)
+                {
+
+                    //generates a random number for each element in array
+                    gameArray[row, col] = random.Next(0, 2);
+                }
+            }
+
+            //checks numbers and displays either "X" if ==1 or "O" if !=1
+            if (gameArray[0, 0] == 1)
+            {
+                A1.Text = "X";
+            }
+            
+            else
+            {
+                A1.Text = "O";
+            }
+
+            if (gameArray[0, 1] == 1)
+            {
+                A2.Text = "X";
+            }
+
+            else
+            {
+                A2.Text = "O";
+            }
+
+            if (gameArray[0, 2] == 1)
+            {
+                A3.Text = "X";
+            }
+
+            else
+            {
+                A3.Text = "O";
+            }
+
+            if (gameArray[1, 0] == 1)
+            {
+                B1.Text = "X";
+            }
+
+            else
+            {
+                B1.Text = "O";
+            }
+
+            if (gameArray[1, 1] == 1)
+            {
+                B2.Text = "X";
+            }
+
+            else
+            {
+                B2.Text = "O";
+            }
+
+            if (gameArray[1, 2] == 1)
+            {
+                B3.Text = "X";
+            }
+
+            else
+            {
+                B3.Text = "O";
+            }
+
+            if (gameArray[2, 0] == 1)
+            {
+                C1.Text = "X";
+            }
+
+            else
+            {
+                C1.Text = "O";
+            }
+
+            if (gameArray[2, 1] == 1)
+            {
+                C2.Text = "X";
+            }
+
+            else
+            {
+                C2.Text = "O";
+            }
+
+            if (gameArray[2, 2] == 1)
+            {
+                C3.Text = "X";
+            }
+
+            else
+            {
+                C3.Text = "O";
+            }
+        }
+
+        //runs the checkScore method when clicked
+        private void checkScoresButton_Click(object sender, EventArgs e)
+        {
+            checkScore();
+        }
+
+        //closes the application
+        private void exitMenu_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
